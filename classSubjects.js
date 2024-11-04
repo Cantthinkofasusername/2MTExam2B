52f014ca55e0418a86b9b3d5e63af4539475778e
Your Name <shado@LAPTOP-U7GG9IKM>
2024-11-04 20:20:36 +0800

    initial commit

---
+++ classSubjects.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Calculus 2";
+let classSchedule = "8AM - 9AM, Tuesday and Thursday";
+let classRoom = "C102";
+let classInstructor = "Ivan Roland";
+let studentName = "Pierce Riley";
+
+console.log("{studentName} is currently enrolled in {subjectTitle} with a classSchedule of at room {classRoom}. The instructor for the subject is {classInstructor}")
